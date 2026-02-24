ce670ca300c1992c7ff823067126ef091a98ffec
Artur Dzida <artur.dzida@gmail.com>
2026-02-24 20:34:58 +0000

    basics for services and autowiring

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class TestController extends AbstractController
 {
     public function welcome(): Response
-    {
+    {        
         return $this->render('Test/welcome.html.twig', [
             'welcome_message' => 'Hello World',
         ]);
+    }
+
+/*
+ * because of autowiring in services.yml I can inject monolog logger here
+ * I know that LoggerInterface should be used by using:
+ * ./bin/console debug:autowiring | grep log
+ * or
+ * ./bin/console debug:container | grep log
+ * Autowiring automatically creates services when needed. I do not need to manually
+ * add and config them in services.yml
+ */
+
+    public function logIt(LoggerInterface $logger): Response
+    {
+        $logger->info('test::welcome invoked');
+        $this->addFlash('success', 'log added');
+        return $this->render('Test/log_it.html.twig', []);
     }
 }
